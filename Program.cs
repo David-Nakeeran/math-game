360ff82866a0f7b02a611d7e1edad08c286e75cb
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-08-04 11:19:22 +0100

    Setup main program structure
    - Initialised Menu instance
    - Implemented GetName method to captures user's
    first name with validation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MathGame;
+
+Menu menu = new Menu();
+
+DateTime date = DateTime.UtcNow;
+string? name = GetName();
+
+menu.ShowMenu(name, date);
+
+string GetName()
+{
+    Console.WriteLine("Please enter your first name");
+    string? name = Console.ReadLine();
+    while (String.IsNullOrWhiteSpace(name))
+    {
+        Console.WriteLine("Please type your first name without spaces");
+        name = Console.ReadLine();
+    }
+    Console.Clear();
+    return name.Trim();
+
+}
